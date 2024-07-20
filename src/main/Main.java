9698e0ba2a1eb3bd9ac8f0b7d62d8f720a0e493f
Breno-Lima <breno.souza@academico.ufpb.br>
2024-07-20 20:44:06 -0300

    fix: path main code

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,11 @@ import lexical.Token;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner("teste.mc");
+        Scanner sc = new Scanner("source_code.mc");
         Token tk;
-        while(true) {
+        while (true) {
             tk = sc.nextToken();
-            if(tk == null) {
+            if (tk == null) {
                 break;
             }
             System.out.println(tk);
